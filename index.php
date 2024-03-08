29ce682b8df1e07f56fa4438fd59f8c566629074
Tien Nguyen <tiennguyen3354@gmail.com>
2024-03-07 21:03:06 -0500

    finished setting up the environment

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+// this will be the controller page .
+//this project will be working with FAT FREE .
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Require the autoload file we get from FAT FREE FRAMEWORK
+require_once ('vendor/autoload.php');
